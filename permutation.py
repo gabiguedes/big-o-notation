619efe0e3d0807ae9f0d5a1ffc9720054e498b01
Gabriela Guedes <zooiv3rde@gmail.com>
2021-06-03 20:19:35 -0300

    add permutation

---
+++ permutation.py
@@ -0,0 +1,15 @@
+def Permutation(data, n):
+    if n == 1:
+        print(data)
+        return
+    for i in range(n):
+        Permutation(data, n - 1)
+        if n % 2 == 0:
+            data[i], data[n-1] = data[n-1], data[i]
+        else:
+            data[0], data[n-1] = data[n-1], data[0]
+data = [1, 2]
+Permutation(data, len(data))
+
+#O(n!) - fatorial
+            
