fd44f9b9d43671bc1dd32ba48e948b3d6f4585df
Gabriela Guedes <zooiv3rde@gmail.com>
2021-06-12 21:44:58 -0300

    #O(n!) - fatorial

--- permutation.py
+++ permutation.py
@@ -1,14 +1,14 @@
-def Permutation(data, n):
+def permutation(data, n):
     if n == 1:
         print(data)
         return
     for i in range(n):
-        Permutation(data, n - 1)
+        permutation(data, n - 1)
         if n % 2 == 0:
             data[i], data[n-1] = data[n-1], data[i]
         else:
             data[0], data[n-1] = data[n-1], data[0]
 data = [1, 2]
-Permutation(data, len(data))
+permutation(data, len(data))
 
 #O(n!) - fatorial           
